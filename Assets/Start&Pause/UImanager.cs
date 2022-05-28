b1f13a26c32cd352f1d2628938a35b131410d98d
snow-QingYang <1047867934@qq.com>
2022-05-29 00:49:29 +0800

    Add StartMenu and PauseUI

---
+++ Assets/Start&Pause/UImanager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UImanager : MonoBehaviour
+{
+    public void ResetGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void QuitGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void ReturnToGame()
+    {
+        Time.timeScale = 1;
+        Panel.Instance.RemoveCanvas();
+
+    }
+}
